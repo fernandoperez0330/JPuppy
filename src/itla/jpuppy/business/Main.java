5b586ff07ce5522db7c15a762cf269791ed31f0d
Jansel Valentin <20093287@itla.edu.do>
2011-07-21 17:45:57 -0400

    creacion de generalModel,anadidura de los metodos de control de los modelos, de las entidades

--- src/itla/jpuppy/business/Main.java
+++
@@ -1,9 +0,0 @@
-package itla.jpuppy.business;
-
-import itla.jpuppy.utils.TestPersistence;
-
-public class Main {
-    public static void main(String[] args){
-        new TestPersistence();
-    }
-}
