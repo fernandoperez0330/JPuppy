8c4dfea7944c0efef4bbce6a1e3e5dc9d1d8f1f7
Frangeris <frangeris_29@hotmail.com>
2011-07-28 04:02:09 -0400

    Exception de persistencia

--- src/itla/jpuppy/business/QueryManager.java
+++ src/itla/jpuppy/business/QueryManager.java
@@ -1,88 +1,91 @@
 package itla.jpuppy.business;
 
 import itla.jpuppy.datalayer.*;
-import java.util.LinkedList;
-
 import java.util.List;
-import java.util.concurrent.CancellationException;
 import javax.persistence.EntityManager;
 
 public class QueryManager {
 
     private EntityManager entityManager;
-    
 
     public QueryManager() {
         entityManager = EntityManagerCreator.getInstanceEM();
     }
 
     public boolean saveObject(Object object) {
+        boolean status;
+
         entityManager.getTransaction().begin();
-        entityManager.persist(object);
-        entityManager.getTransaction().commit();
-        return true;
+        try {
+            entityManager.persist(object);
+            entityManager.getTransaction().commit();
+            status = true;
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
+            status = false;
+        }
+        return status;
     }
 
     public boolean deleteObject(Object object) {
         boolean state = true;
         entityManager.getTransaction().begin();
-        try{
-        entityManager.remove(object);
-        entityManager.getTransaction().commit();
-        }catch( Exception  e){
+        try {
+            entityManager.remove(object);
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
             state = false;
         }
         return state;
     }
 
     public boolean updateObject(Object object) {
-        boolean state= true;
+        boolean state = true;
         entityManager.getTransaction().begin();
-        try{
-        entityManager.merge(object);
-        entityManager.getTransaction().commit();
-        }catch( Exception f ){
+        try {
+            entityManager.merge(object);
+            entityManager.getTransaction().commit();
+        } catch (Exception f) {
             state = false;
-                    }
+        }
         return state;
     }
-    
-    //Metodos Disponibles para autocomplete en busqueda
-    
-    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
+    //Metodos Disponibles para autocomplete en busqueda
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     public List<Users> searchUser(String name) {
-       List<Users> listUsers =  entityManager.createQuery("SELECT a FROM Users a WHERE a.name LIKE :nameToFind").setParameter("nameToFind", name).getResultList();
-       return listUsers;
+        List<Users> listUsers = entityManager.createQuery("SELECT a FROM Users a WHERE a.name LIKE :nameToFind").setParameter("nameToFind", name).getResultList();
+        return listUsers;
     }
 
     //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     public List<Customers> searchCustomer(String name) {
-        List<Customers> listCustomers =  entityManager.createQuery("SELECT a FROM Customers a WHERE a.name LIKE :nameToFind").setParameter("nameToFind", name).getResultList();
+        List<Customers> listCustomers = entityManager.createQuery("SELECT a FROM Customers a WHERE a.name LIKE :nameToFind").setParameter("nameToFind", name).getResultList();
         return listCustomers;
     }
 
     //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 
     public List<Patients> searchPatient(String patientsId) {
-         List<Patients> listPatients =  entityManager.createQuery("SELECT a FROM Patients a WHERE a.patientsId LIKE :nameToFind").setParameter("nameToFind", patientsId).getResultList();
+        List<Patients> listPatients = entityManager.createQuery("SELECT a FROM Patients a WHERE a.patientsId LIKE :nameToFind").setParameter("nameToFind", patientsId).getResultList();
         return listPatients;
     }
 
     //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 
     public List<Species> searchSpecie(String name) {
-         List<Species> listSpecieses =  entityManager.createQuery("SELECT a FROM Species a WHERE a.speciesName LIKE :nameToFind").setParameter("nameToFind", name).getResultList();
+        List<Species> listSpecieses = entityManager.createQuery("SELECT a FROM Species a WHERE a.speciesName LIKE :nameToFind").setParameter("nameToFind", name).getResultList();
         return listSpecieses;
     }
 
     //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 
     public List<Breeds> searchBreeds(String name) {
-        List<Breeds> listBreeds =  entityManager.createQuery("SELECT a FROM Breeds a WHERE a.breedsName LIKE :nameToFind").setParameter("nameToFind", name).getResultList();
+        List<Breeds> listBreeds = entityManager.createQuery("SELECT a FROM Breeds a WHERE a.breedsName LIKE :nameToFind").setParameter("nameToFind", name).getResultList();
         return listBreeds;
-       
+
     }
-    @Override 
-      protected void finalize(){
+
+    @Override
+    protected void finalize() {
         entityManager.close();
     }
-   
+
 }
