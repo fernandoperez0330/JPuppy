c0f7772e0b79f6cd5bf95cb8bba0ce0325c9242b
Fernando Perez <20093331@itla.edu.do>
2011-07-28 19:03:21 -0400

    creacion del archivo 'homeFrame'

--- src/itla/jpuppy/forms/HomeFrame.java
+++ src/itla/jpuppy/forms/HomeFrame.java
@@ -1,4 +1,23 @@
 package itla.jpuppy.forms;
 
-public class HomeFrame {
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class HomeFrame implements FrameOption {
+    JFrame frame = null;
+    
+    @Override
+    public void showFrame() {
+       frame = new JFrame("JPuppy Sistema de veterinaria");
+       frame.setSize(new Dimension(400,400));
+       frame.setLocationRelativeTo(null);
+       frame.setVisible(true);
+    }
+
+    @Override
+    public void closeFrame() {
+       frame.setVisible(false);
+       frame.dispose();
+    }
+    
 }
