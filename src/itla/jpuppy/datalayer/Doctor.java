fc321418312b3e557b1855f37b0211c2b1472855
Jordhano <jordhano@hotmail.com>
2011-07-30 22:56:55 +0200

    Update

--- src/itla/jpuppy/datalayer/Doctor.java
+++ src/itla/jpuppy/datalayer/Doctor.java
@@ -6,7 +6,6 @@ package itla.jpuppy.datalayer;
 
 import java.io.Serializable;
 import java.util.List;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
--- src/itla/jpuppy/models/SearchingCtrlCustomers.java
+++ src/itla/jpuppy/models/SearchingCtrlCustomers.java
@@ -2,23 +2,26 @@ package itla.jpuppy.models;
 
 import itla.jpuppy.datalayer.Customers;
 
+/**
+ *
+ * @author Jordhano
+ */
 public class SearchingCtrlCustomers implements SearchingControl {
 //new String[] {"Id","Name","Lastname","City"}
-
     @Override
     public Object getValueAt(Object x, int y) {
         Customers temp = (Customers) x;
         switch (y) {
             case 0:
-                //return temp.getCustomersId();
+                return temp.getPersonId();
             case 1:
                 return temp.getName();
             case 2:
-                //return temp.getLastname();
+                return temp.getLastName();
             case 3:
                 return temp.getCity();
         }
-
+        
         return null;
     }
 }
