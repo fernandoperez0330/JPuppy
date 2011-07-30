203a3242880f17867b2fc9b77459856107992ddf
Fernando Perez <20093331@itla.edu.do>
2011-07-30 09:35:09 -0400

    Creacion del frame 'Acerca de...' para el sistema

---
+++ src/itla/jpuppy/forms/AboutUsFrame.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package itla.jpuppy.forms;
+
+/**
+ *
+ * @author Fernando
+ */
+public class AboutUsFrame implements FrameOption {
+
+    @Override
+    public void showFrame() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void closeFrame() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
