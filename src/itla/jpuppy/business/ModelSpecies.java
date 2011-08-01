15f933176c5f87ba4daa70e7382149f297d1d62d
Frangeris <frangeris_29@hotmail.com>
2011-08-01 00:57:00 -0400

    Captura instancia..

--- src/itla/jpuppy/business/ModelSpecies.java
+++ src/itla/jpuppy/business/ModelSpecies.java
@@ -19,36 +19,33 @@ public class ModelSpecies implements GeneralModel {
 
     //retorna un objeto especifico tipo Species cuando se llame debe cargarse el Id desde generalVariableID del paquete util
     //los datos se le asignan cuando el usuario selecciona un cliente  del table
-    
     public Species searchSpecies(int id) {
         EntityManager entityManager = EntityManagerCreator.getInstanceEM();
-        Species temp = entityManager.find( Species.class, id );
+        Species temp = entityManager.find(Species.class, id);
         return temp;
     }
 
     // retorna lista de patient para autocomplete
-    
-    public List<Species> searchAllSpeciesByName(String name) { 
+    public List<Species> searchAllSpeciesByName(String name) {
         return queryManager.searchSpecie(name);
     }
 
     //Metodos comunes a todos los modelos , se llama el correspondiente de  queryManager
-    
     @Override
     public boolean insertObject(Object object) {
-        Species g = ( Species ) object;
-        return queryManager.saveObject( g );
+        Species g = (Species) object;
+        return queryManager.saveObject(g);
     }
 
     @Override
     public boolean updateObject(Object object) {
-        Species g = ( Species ) object;
-        return queryManager.updateObject( g );
+        Species g = (Species) object;
+        return queryManager.updateObject(g);
     }
 
     @Override
     public boolean deleteObject(Object object) {
-        Species g = ( Species )object;      
-        return queryManager.deleteObject( g );
+        Species g = (Species) object;
+        return queryManager.deleteObject(g);
     }
 }
